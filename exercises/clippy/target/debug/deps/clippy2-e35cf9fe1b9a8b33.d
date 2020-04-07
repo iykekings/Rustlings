93500adc15087bcb77159035b64ec4c8bab7aae6
iykekings <iykekings36@gmail.com>
2020-04-07 22:26:34 +0100

    completed

---
+++ exercises/clippy/target/debug/deps/clippy2-e35cf9fe1b9a8b33.d
@@ -0,0 +1,5 @@
+/Users/ikechukwu/Desktop/Test/Rustlings/exercises/clippy/target/debug/deps/clippy2-e35cf9fe1b9a8b33.rmeta: clippy2.rs
+
+/Users/ikechukwu/Desktop/Test/Rustlings/exercises/clippy/target/debug/deps/clippy2-e35cf9fe1b9a8b33.d: clippy2.rs
+
+clippy2.rs:
